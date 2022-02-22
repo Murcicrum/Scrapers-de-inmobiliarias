9a2d3a9bac4fd98d58a3c8b1b7e0b6676198afb1
Murcicrum <murcicrum@gmail.com>
2022-02-21 21:05:53 -0300

    Nombre acortado

--- scrap_properati.py
+++ scrap_properati.py
@@ -192,7 +192,7 @@ if __name__=='__main__':
               'deptos': URL_SEARCH_DEPTOS }
 
     for tipo, url in SEARCH.items():
-        TODAY = time.strftime( "%Y-%m-%d-%H:%M", time.localtime() )
+        TODAY = time.strftime( "%Y-%m-%d", time.localtime() )
         FILEPATH = DIRECTORY + '_'.join([TODAY,tipo,'properati.csv'])
 
         print(f'Escrapeando {tipo}.\nLos datos se guardarán en {FILEPATH}')
