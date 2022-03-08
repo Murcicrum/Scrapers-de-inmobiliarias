c7e16a3366f8db64884d82b9a6b4ac6fce6a3c25
Murcicrum <murcicrum@gmail.com>
2022-03-08 10:58:03 -0300

    bug fixed

--- scrap_properati.py
+++ scrap_properati.py
@@ -118,7 +118,7 @@ def get_data(urls: list) -> list:
 
             for r, s in to_extract.items():
                 data[r] = find_value( s, d_props )
-!            data['url']=url
+            data['url']=url
             
             print(i, data)
             
